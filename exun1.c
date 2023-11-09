31358c0be500a93cd2a35fbc8273264a3e11016f
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-11-09 16:52:49 -0300

    Update exun1.c

--- exun1.c
+++ exun1.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 
 float validaQuantidade(float qtdPecas){
+    float adicional = 0;
      if(qtdPecas > 50 && qtdPecas <= 80){
-        
+        adicional += 0.5;
     }else if(qtdPecas > 80){
-        
+        adicional += 0.75;
     }else{}
+    return adicional;
 }
 
 float calculaSalario(float qtdPecas){
     float nSalario = 0;
+    float adicional = validaQuantidade(qtdPecas); 
+
     if(qtdPecas > 50 && qtdPecas <= 80){
-        nSalario = (qtdPecas - 50) * 0.5 + 600;
+        nSalario = (qtdPecas - 50) * adicional + 600;
     }else if(qtdPecas > 80){
-        nSalario = (qtdPecas - 80) * 0.75 + 600 + 15;
-    }else{}
+        nSalario = (qtdPecas - 80) * adicional + 600 + 15;
+    }else{
+        nSalario = 600;
+    }
     return nSalario;
 }
 
 float mostraFinal(float qtdPecas, float nSalario){
-    printf("%f", nSalario);
+    if(qtdPecas != -1){
+    printf("Salario com o bonus: [%.2f]", nSalario);
+    }
     return nSalario;
 }
 
@@ -28,12 +36,12 @@ int main(){
    
 
     do{
-    printf("\nDigite a quantidade de pecas produzidas ou -1 para encerrar: ");
+    printf("\n\nDigite a quantidade de pecas produzidas ou -1 para encerrar: ");
     scanf("%f", &qtdPecas);
 
     float nSalario = calculaSalario(qtdPecas);
     mostraFinal(qtdPecas, nSalario);
-   
-    } while (qtdPecas != -1);
+
+    } while(qtdPecas != -1);
     return 0;
 }
