049c9c990265eeea07016cc8e8a80ef4786d5bb3
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-11-11 17:42:20 -0300

    Update exun2.c

--- exun2.c
+++ exun2.c
@@ -1,25 +1,73 @@
-/*2.	Com a proposta para ampliação da terceirização da mão de obra,
-o Ministério da Fazenda está empenhado em elaborar uma análise sobre o
-recebimento de salário regular através do registro em carteira profissional 
-dos funcionários no país. Assim, elabore um algoritmo que obtenha,
-para cada assalariado, com carteira profissional assinada,
-o seu sexo M(masculino) e F(feminino) e o valor de seu salário,
-devendo este salário ser maior que um real (R$1,00). Seu algoritmo deverá 
-analisar e classificar este assalariado em Acima, Igual ou Abaixo do salário mínimo.
-Sabe-se que o salário mínimo nacional é de R$ 1400,00. Após a leitura dos dados de 
-cada assalariado cadastrado pelo ministério deverá ser apresentado o seu salário
-em reais, a classificação por extenso em relação ao salário mínimo e o sexo por
-extenso (Masculino ou Feminino).A classificação em relação ao salário mínimo
-nacional deverá ser feita pelo sub-algoritmo classificaSalario e os resultados
-solicitados para cada assalariado deverão ser apresentados pelo sub-algoritmo 
-denominado mostraClassifica. 
-Você deverá fazer a entrada de dados para todos os assalariados que foram pesquisados 
-pelo ministério, lembrando de fazer sempre a validação de todos os dados informados
-em sub-algoritmos específicos.   
-Na solução deste problema os resultados finais de cada assalariado que participou desta
-pesquisa deverão ser apresentados.
-Após a leitura dos dados de todos os assalariados pesquisados o seu algoritmo deverá
-apresentar a quantidade de assalariados com salário abaixo do salário mínimo e a 
-quantidade assalariados com salário acima do salário mínimo
+#include <stdio.h>
 
-*/
+char validaSexo() {
+    char sexo;
+    do {
+        printf("Informe o sexo (M/F): ");
+        scanf(" %c", &sexo);
+    } while (sexo != 'M' && sexo != 'm' && sexo != 'F' && sexo != 'f');
+    return sexo;
+}
+
+float validaSalario() {
+    float salario;
+    do {
+        printf("Informe o salario: R$ ");
+        scanf("%f", &salario);
+    } while (salario <= 1.0);
+    return salario;
+}
+
+char classificaSalario(float salario) {
+    if (salario > 1400.0) {
+        return 'A';
+    } else if (salario == 1400.0) {
+        return 'I';
+    } else {
+        return 'B';
+    }
+}
+
+void mostraClassificacao(char sexo, float salario, char classificacao) {
+    printf("\nSexo: %s, Salario: R$ %.2f, Classificação: ", (sexo == 'M' || sexo == 'm') ? "Masculino" : "Feminino", salario);
+    switch (classificacao) {
+        case 'A':
+            printf("Acima do salario minimo\n");
+            break;
+        case 'I':
+            printf("Igual ao salario minimo\n");
+            break;
+        case 'B':
+            printf("Abaixo do salario minimo\n");
+            break;
+    }
+}
+
+int main() {
+    int numAssalariados, abaixoSalarioMinimo = 0, acimaSalarioMinimo = 0;
+
+    printf("Informe o numero de assalariados: ");
+    scanf("%d", &numAssalariados);
+
+    for (int i = 0; i < numAssalariados; i++) {
+        printf("\nAssalariado %d:", i + 1);
+
+        char sexo = validaSexo();
+        float salario = validaSalario();
+        char classificacao = classificaSalario(salario);
+
+        mostraClassificacao(sexo, salario, classificacao);
+
+        if (classificacao == 'A') {
+            acimaSalarioMinimo++;
+        } else if (classificacao == 'B') {
+            abaixoSalarioMinimo++;
+        }
+    }
+
+    printf("\nResumo:\n");
+    printf("Assalariados abaixo do salario minimo: %d\n", abaixoSalarioMinimo);
+    printf("Assalariados acima do salario minimo: %d\n", acimaSalarioMinimo);
+
+    return 0;
+}
